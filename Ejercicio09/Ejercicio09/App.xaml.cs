1c411889afc165716f97be306de3c6000edd5e89
jhoncabrera <jhoncabrera2050@gmail.com>
2023-05-31 22:36:57 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Ejercicio09/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new TapDemo();
+            //MainPage = new PinchDemo();
+            //MainPage = new PanDemo();
+            MainPage = new SwipeDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejercicio09/Ejercicio09/TapDemo.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio09
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TapDemo : ContentPage
+    {
+        int tapCount;
+        readonly Label label;
+        public TapDemo()
+        {
+            InitializeComponent();
+            var image = new Image
+            {
+                Source = "gato1.png",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.NumberOfTapsRequired = 2;
+            tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
+            image.GestureRecognizers.Add(tapGestureRecognizer);
+
+            label = new Label
+            {
+                Text = "tap the photo!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20, 100),
+                Children =
+                {
+                    image,
+                    label
+                }
+            };
+        }
+        private void OnTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            tapCount++;
+            label.Text = String.Format("{0} tap{1} so far!",
+                tapCount,
+                tapCount == 1 ? "" : "s");
+
+            var imageSender = (Image)sender;
+
+            if (tapCount % 2 == 0)
+            {
+                imageSender.Source = "gato1.jpg";
+            }
+            else
+            {
+                imageSender.Source = "gato2.png";
+            }
+        }
+    }
+}
